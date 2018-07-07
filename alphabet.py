1fda1c1636c4a9fab10ce11cd340013e1a3009e1
Venureddie <40885700+Venureddie@users.noreply.github.com>
2018-07-07 10:25:37 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,5 @@
-ch=raw_input()
-bh=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-if ch in bh:
-  print "Alphabet"
- else:
-  print "No"
+ch = raw_input()
+if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+    print("Alphabet")
+else:
+    print("Not an Alphabet")
